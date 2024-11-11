1bbd5490ee77128b9b76d24f433ea47a7b8687f7
jorgerodriguezg1988 <jorgerodriguezg1988@gmail.com>
2024-11-11 18:38:29 -0500

    Creación del proyecto, estructuración del Page Object Model POM y creación del primer método para confirmar su funcionamiento.

---
+++ cypress/pageObjects/home.js
@@ -0,0 +1,9 @@
+class Home {
+    
+    gotoCreateAccountButton() {
+        cy.get('#html-body > div.page-wrapper > header > div.panel.wrapper > div > ul > li').contains('Create an Account').click()
+
+    }
+  }
+  
+  export default Home;
